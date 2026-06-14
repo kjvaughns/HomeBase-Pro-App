7397aff4b734cbe6dc26ed01547d1d78a2af75c0
kjvaughns <kjvaughns13@gmail.com>
2026-06-14 01:02:10 +0000

    fix: config plugin to remove sentry upload build phase

--- artifacts/homebase/plugins/withSentryBuildPhaseRemoval.js
+++ artifacts/homebase/plugins/withSentryBuildPhaseRemoval.js
@@ -1,37 +1,21 @@
-const { withXcodeProject } = require('expo/config-plugins');
+const { withXcodeProject } = require('@expo/config-plugins');
 
 module.exports = function withSentryBuildPhaseRemoval(config) {
   return withXcodeProject(config, (config) => {
     const xcodeProject = config.modResults;
-    const objects = xcodeProject.hash.project.objects;
-    const shellScripts = objects['PBXShellScriptBuildPhase'] || {};
-
-    const sentryUUIDs = Object.keys(shellScripts).filter((uuid) => {
-      if (uuid.endsWith('_comment')) return false;
-      const phase = shellScripts[uuid];
-      return (
-        phase &&
-        typeof phase.name === 'string' &&
-        phase.name.includes('Upload Debug Symbols to Sentry')
-      );
+    const targets = xcodeProject.hash.project.objects.PBXNativeTarget || {};
+    
+    Object.values(targets).forEach((target) => {
+      if (!target.buildPhases) return;
+      target.buildPhases = target.buildPhases.filter((phaseRef) => {
+        const key = phaseRef.value || phaseRef;
+        const scripts = xcodeProject.hash.project.objects.PBXShellScriptBuildPhase || {};
+        const phase = scripts[key];
+        if (!phase) return true;
+        const name = (phase.name || '').replace(/"/g, '');
+        return !name.includes('Upload Debug Symbols to Sentry');
+      });
     });
-
-    if (sentryUUIDs.length === 0) return config;
-
-    const nativeTargets = objects['PBXNativeTarget'] || {};
-    for (const targetKey of Object.keys(nativeTargets)) {
-      if (targetKey.endsWith('_comment')) continue;
-      const target = nativeTargets[targetKey];
-      if (!target || !Array.isArray(target.buildPhases)) continue;
-      target.buildPhases = target.buildPhases.filter(
-        (phase) => !sentryUUIDs.includes(phase.value)
-      );
-    }
-
-    for (const uuid of sentryUUIDs) {
-      delete shellScripts[uuid];
-      delete shellScripts[`${uuid}_comment`];
-    }
 
     return config;
   });
